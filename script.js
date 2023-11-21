5500e4fa534e918ecdceb1a374f1e64185aec293
testWordpressGit <109813970+testWordpressGit@users.noreply.github.com>
2023-11-21 21:29:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,39 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	let sum=0;
+	
+	while(arr.length>0)
+		{
+			arr.sort((a,b)=>{
+				return a-b;
+			});
+
+			let num1=arr[0];
+			
+			if(arr.length>=2)
+			{
+				num1+=arr[1];
+			}
+
+			sum+=num1;
+
+			arr.shift();
+			arr.shift();
+
+			if(arr.length<=0)
+			{
+				break;
+			}
+			
+			arr.push(num1);
+			
+		}
+
+	return sum;
+  
+}
+
   
 }
 
